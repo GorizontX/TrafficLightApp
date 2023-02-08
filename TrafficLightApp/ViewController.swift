c9da5dedb67addeb4636148a5915faf6379acb49
Andrey <65368541+GorizontX@users.noreply.github.com>
2023-02-08 19:36:23 +0100

    1. Added elements on the screen 2. Linked the elements on the screen with the code 3. Set up the elements on the screen 4. Set up the logic of pressing the button

--- TrafficLightApp/ViewController.swift
+++ TrafficLightApp/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redViewLabel: UIView!
+    @IBOutlet var yellowViewLabel: UIView!
+    @IBOutlet var greenViewLabel: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redViewLabel.layer.cornerRadius = redViewLabel.frame.height / 2
+        yellowViewLabel.layer.cornerRadius = yellowViewLabel.frame.height / 2
+        greenViewLabel.layer.cornerRadius = greenViewLabel.frame.height / 2
+        
+        redViewLabel.alpha = 0.3
+        yellowViewLabel.alpha = 0.3
+        greenViewLabel.alpha = 0.3
+        
     }
 
+    @IBAction func startButtonPressed() {
+        
+        startButton.setTitle("NEXT", for: .normal)
+        
+        if redViewLabel.alpha == 1 {
+            redViewLabel.alpha = 0.3
+            yellowViewLabel.alpha = 1
+        } else if yellowViewLabel.alpha == 1 {
+            yellowViewLabel.alpha = 0.3
+            greenViewLabel.alpha = 1
+        } else {
+            greenViewLabel.alpha = 0.3
+            redViewLabel.alpha = 1
+        }
+        
+    }
+    
 
 }
 
